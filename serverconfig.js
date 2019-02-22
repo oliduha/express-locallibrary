a52244c501289b6e7fd22c856f807ed361dc3739
oli406 <oliduha@gmail.com>
2019-02-22 16:06:18 +0100

    Rewrite env variables in ./serverconfig.js

---
+++ serverconfig.js
@@ -0,0 +1,26 @@
+/* eslint-disable linebreak-style */
+
+var port = process.env.PORT || process.env.PORT_AZURE || process.env.PORT_AWS || 8080;
+
+// OpenShift ports
+if(process.env.OPENSHIFT_INTERNAL_PORT) {
+  port = process.env.OPENSHIFT_INTERNAL_PORT;
+}
+if(process.env.OPENSHIFT_NODEJS_PORT) {
+  port = process.env.OPENSHIFT_NODEJS_PORT;
+}
+
+// Override the ports
+if(process.env.OVERRIDE_PORT) {
+  port = process.env.CUSTOM_PORT;
+}
+
+// Variable name environment variable for the port
+if(process.env.VARIABLE_PORT) {
+  port = process.env[process.env.VARIABLE_PORT_NAME];
+}
+
+module.exports = {
+  serverPort: port,
+  applicationInsightsInstrumentationKey: process.env.appInsightsKey
+};
